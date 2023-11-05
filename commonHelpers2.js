6743a8919766472fb7aeae0814a371739fcdbc50
salandr <127801413+salandr@users.noreply.github.com>
2023-11-05 22:14:08 +0000

    Deploying to gh-pages from @ salandr/goit-advancedjs-hw-01@09da9aae9b4b2cd29e564582578ca8bb9f7a961d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-d78ab9e0.js";const e="videoplayer-current-time",l=document.querySelector("#vimeo-player"),t=new r(l),n=function(o){localStorage.setItem(e,o.seconds)};t.on("timeupdate",a(n,1e3));t.setCurrentTime(JSON.parse(localStorage.getItem(e))||0);
+//# sourceMappingURL=commonHelpers2.js.map
