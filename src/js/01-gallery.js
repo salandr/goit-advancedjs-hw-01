5132c47e89e988a934de66ed01dd0bc05189de76
salandr <ansaliy200013@gmail.com>
2023-11-05 21:37:06 +0000

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.js-gallery');
+
+function markupGallery(arrImages) {
+  return arrImages
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', markupGallery(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
